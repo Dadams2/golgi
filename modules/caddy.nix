2cf3b4b651849742c03f7cc0d6026d8bc2796ca6
David Adams <david@dadams2.com>
2025-07-18 21:21:31 +1000

    have the ability to only use ipv6 for ddns

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -7,6 +7,7 @@ let
             ${config.site.domain} @ * ${config.site.cloudflare-bypass-subdomain}
     }
     ip_source simple_http https://icanhazip.com
+    versions ${config.site.ipversions}
     ''
     else
     ''
