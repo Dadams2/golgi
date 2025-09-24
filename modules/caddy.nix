6088ddba85406f08e5f3da985858cc7f8510247d
dadams <github@dadams2.com>
2025-09-25 00:01:18 +0800

    fix bad rebase

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -7,7 +7,6 @@ let
             ${config.site.domain} @ * ${config.site.cloudflare-bypass-subdomain}
     }
     ip_source simple_http https://icanhazip.com
-    versions ${config.site.server.ipversions}
     ''
     else
     ''
